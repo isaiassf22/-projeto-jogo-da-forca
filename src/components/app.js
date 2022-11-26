ea123f2ebf765037b3ce79d36d7b3297d89e2381
isaias <figueiredo.isaias@gmail.com>
2022-11-25 23:04:40 -0300

    updated version

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,6 @@
 
-import "./style.css"
-import { palavraEscolhida } from "./palavras"
-import { caracteres } from "./palavras"
 import { alfabeto } from "./letras";
+import { semAcento } from "./palavras";
 import forc0 from "./img/forca0.png"
 import forc1 from "./img/forca1.png"
 import forc2 from "./img/forca2.png"
@@ -11,150 +9,161 @@ import forc4 from "./img/forca4.png"
 import forc5 from "./img/forca5.png"
 import forc6 from "./img/forca6.png"
 import React from "react";
+import GlobalStyle from "./style/globalStyle";
+import styled from "styled-components";
 
+let correct = 0
+let factor = ''
+let wrong = 0
 
 
 export default function Jogar() {
 
+
     //apertar o escolher palavra para liberar o jogo
     const [selected, setSelected] = React.useState(false)
-    function chooseWord() {
-        setSelected(true)
-        Palavras()
-    }
-    
+
+
     //selecionar as fotos de acordo com os erros
     const [photos, setPhotos] = React.useState(forc0)
     //verificar numero de erros
-    const [erro,setErro]=React.useState(0)
+    const [erro, setErro] = React.useState(0)
+
+    const [perdeu, setPerdeu] = React.useState(false)
+
+    const [ganhou, setGanhou] = React.useState(false)
+
+    const [desabilitaInput, setDesabilitaInput] = React.useState(true)
+
+    const [desabilitaTeclado, setDesabilitaTeclado] = React.useState(alfabeto)
+
+    const [caracteres, setArrayPalavra] = React.useState([])
+    const [palavraEscolhida, setPalavraEscolhida] = React.useState()
+
+    const [selectLetters, setSelectLetters] = React.useState([])
     
-    const [acerto,setAcerto]=React.useState(0)
-
-    const [perdeu,setPerdeu]=React.useState(false)
-
-    const[ganhou,setGanhou]=React.useState(false)
-
-    let tracos = []
-
-    let factor=''
-    const [selectLetters, setSelectLetters]=React.useState([])
+    const [result,setResult]=React.useState("letra")
 
     function Palavras() {
-        
-        console.log(palavraEscolhida)
 
-        let quantidadeDeLetras = palavraEscolhida.length
-        const traco = <div className="traco"></div>
-        console.log(caracteres)
-        function numerTracos() {
-            for (let i = 0; i < quantidadeDeLetras; i++) {
-                tracos.push(traco)
-            }
+        setSelected(true)
+        setDesabilitaInput(false)
+        setDesabilitaTeclado([])
+        const palavra = semAcento[Math.floor(Math.random() * (semAcento.length - 1))]
+        setPalavraEscolhida(palavra)
+        console.log(palavra)
+        const arrayP = palavra.split('')
+        console.log(arrayP)
+        setArrayPalavra(arrayP)
 
-        }
-        numerTracos()
-        return (
-            <>
-                {tracos}
-            </>
-        )
     }
-    const [result,currentResult]=React.useState(tracos)
 
-    
-    function Letters(){
-        function letter (props){
-            factor=props
+    function tryAgain() {
+        setArrayPalavra([])
+        setSelectLetters([])
+        setErro(0)
+        setPhotos(forc0)
+        Palavras()
+        correct = 0
+        wrong=0
+    }
+
+
+    function Letters() {
+        function letter(props) {
+            factor = props
             console.log(factor)
             setSelectLetters([...selectLetters, factor])
-            for (let i=0;i<caracteres.length;i++){
-                if(caracteres[i]===factor){
-                    setAcerto(acerto+1)
-                    console.log(`achei um ${factor}`)
-                    console.log(`acertei ${acerto+1} vezes`)
-                    console.log(i)
-                    console.log(tracos)
-                    tracos[i]= <div class='letra'>${factor}</div>
-                    
-                }
-                currentResult(tracos)
-                
+            setDesabilitaTeclado([...desabilitaTeclado, factor])
+            let incidence = caracteres.filter(function fill(el) { if (el === factor) { return factor } })
+            if (caracteres.includes(factor)) {
+                correct += incidence.length
+                incidence.map(() => console.log(`achei um ${factor}`))
+
+                console.log(correct)
+
+
             }
-            if((acerto+1)===caracteres.length){
-                setGanhou(true)
-                setTimeout(()=>alert('voce ganhou!!'),500)
-                setTimeout(()=> window.location.reload(false),2000)
+
+            if (caracteres.includes(factor) === false) {
+                setErro(erro + 1)
+                wrong += 1
+                const listImg = [forc1, forc2, forc3, forc4, forc5, forc6]
+                setPhotos(listImg[erro])
+                console.log(`esta no ${wrong} erro`)
+
             }
 
 
 
-            if (palavraEscolhida.includes(factor)===false){
-                setErro(erro+1)
-           const listImg=[forc1,forc2,forc3,forc4,forc5,forc6]
-           setPhotos(listImg[erro])
-            /*switch (erro) {
-                case erro === 1:
-                    setPhotos(forc1)
-                    break;
-                case erro === 2:
-                    setPhotos(forc2)
-                    break;
-                case erro === 3:
-                    setPhotos(forc3)
-                    break;
-                case erro === 4:
-                    setPhotos(forc4)
-                    break;
-                case erro === 5:
-                    setPhotos(forc5)
-                    break;
-                case erro === 6:
-                    setPhotos(forc6)
-                    break;
-                    default:
-                        
-            }*/
 
-            console.log(`esta no ${erro+1} erro`)
-            }
-            if ((erro+1)===6){
-                setPerdeu(true)
-                setTimeout(()=>alert('voce perdeu!!'),500)
-                setTimeout(()=> window.location.reload(false),2000)
-            }
-            
+
+            validation()
+
         }
-        //const renderAlfabeto=alfabeto.map((n)=> <button onClick={()=>letter(n)}>{n}</button>)
-        return(
-            <div className="botoes">
-                {alfabeto.map((n)=> <button key={n} onClick={()=>letter(n)}>{n}</button>)}
-            </div>
+
+        function validation() {
+            console.log(`acertei ${correct} vezes`)
+
+            if (correct === caracteres.length) {
+                setGanhou(true)
+                setTimeout(() => alert('voce ganhou!!'), 1000)
+                setTimeout(() => window.location.reload(), 3000)
+                setResult("ganhou")
+                setDesabilitaTeclado(alfabeto)
+            }
+
+
+
+
+
+
+        }
+        if ((erro) === 6) {
+            setPerdeu(true)
+            setTimeout(() => alert('voce perdeu!!'), 1000)
+            setTimeout(() => window.location.reload(), 3000)
+            setResult("perdeu")
+            setDesabilitaTeclado(alfabeto)
+        }
+        return (
+            <Botoes>
+                {alfabeto.map((n) => <button key={n} onClick={() => letter(n)} disabled={desabilitaTeclado.includes(n)} >{n}</button>)}
+            </Botoes>
+
         )
     }
 
-    function Chutar(){
-        const [keyWord,setKeyWord]=React.useState("")
-        
-        function Check(){
+    function Chutar() {
+        const [keyWord, setKeyWord] = React.useState("")
+
+        function Check() {
             console.log(palavraEscolhida)
-            console.log(keyWord)  
-            setKeyWord("")    
-            if(keyWord===palavraEscolhida){
-               
-            }else{
+            console.log(keyWord)
+            setKeyWord("")
+            if (keyWord === palavraEscolhida) {
+                setGanhou(true)
+                setTimeout(() => alert('voce ganhou!!'), 1000)
+                setTimeout(() => window.location.reload(), 3000)
+                setResult("ganhou")
+                setDesabilitaTeclado(alfabeto)
+            } else {
                 setPhotos(forc6)
                 setPerdeu(true)
-                setTimeout(()=>alert('voce perdeu!!'),500)
-                setTimeout(()=> window.location.reload(false),2000)
+                setTimeout(() => alert('voce perdeu!!'), 1000)
+                setTimeout(() => window.location.reload(), 3000)
+                setResult("perdeu")
+                setDesabilitaTeclado(alfabeto)
             }
-            
+
         }
-        return(
-            <div className="chute">
+        return (
+            <Chute>
                 <p>já sei a palavra</p>
-                <input placeholder="digite a palavra" value={keyWord} onChange={e=> setKeyWord(e.target.value)} />
+                <input disabled={desabilitaInput} placeholder="digite a palavra" value={keyWord} onChange={e => setKeyWord(e.target.value)} />
                 <button onClick={Check}>chutar</button>
-            </div>
+            </Chute>
+
         )
     }
 
@@ -162,22 +171,120 @@ export default function Jogar() {
 
     return (
         <>
+            <GlobalStyle />
+
             <h1>Bem vindo ao jogo da forca</h1>
-            <main>
+            <Main>
                 <img src={photos} className="forca" alt="forca" />
-                <div className="right-side">
-                    <button className="escolher" onClick={chooseWord} > Escolher palavra</button>
-                    <div className="palavraX">
-                       {selected ? caracteres.map((l,index)=> <p className="letra" key={index}>{selectLetters.includes(l) ? l : '-' }</p>): ''}
-                        {ganhou ? <p className="ganhou">{palavraEscolhida}</p>:'' }
-                        {perdeu ? <p className="perdeu">{palavraEscolhida}</p>: ''}
-                    </div>
+                <Right_side>
+                    <button onClick={selected ? tryAgain : Palavras} > Escolher palavra</button>
+                    <Palavrax>
+                        <div>
+                            {ganhou || perdeu ? <p className={result}>{palavraEscolhida}</p>: caracteres.map((l, index) => <p className={result}  key={index}>{selectLetters.includes(l) ? l : '-'}</p>)}
+                        </div>
+                    </Palavrax>
+                </Right_side>
 
-                </div>
 
-            </main>
-            <Letters/>
-            <Chutar/>
+            </Main>
+            <Letters />
+            <Chutar />
         </>
     )
 }
+
+const Botoes = styled.div`
+    display: flex;
+    max-width: 1000px;
+    flex-wrap: wrap;
+    button{
+    width: 50px;
+    height: 50px;
+    margin: 10px;
+    background-color: rgba(165, 158, 158, 0.253);
+}
+`
+const Chute = styled.div`
+    display: flex;
+    max-width: 1000px;
+    button{
+    width: 80px;
+    height: 50px;
+    background-color: aqua;
+    margin: auto 0 ;
+    margin-left: 10px;
+    border-radius: 5px;
+    font-size: 16px;
+    }
+    p{
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 10px;
+    }
+    input{
+    width: 350px;
+    height: 35px;
+    margin: auto 0;
+    text-align: center;
+    font-size: 16px;
+    }
+
+`
+const Main = styled.div`
+     display: flex;
+    position: relative;
+    background-color: aquamarine;
+    button{
+    width: 230px;
+    height: 60px;
+    font-size: 17px;
+    background-color: rgb(89, 255, 47);
+    color: #fff;
+    border-radius: 5px;
+    margin: 70px 100px;
+}
+
+`
+const Right_side = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const Palavrax = styled.div`
+    margin: auto 0;
+    margin-left: 100px;
+    display: flex;
+    
+    div{
+    display: flex;
+    
+    
+    }
+   
+    p.traco{
+        width: 50px;
+        height: 4px;
+        background-color: black;
+        border-radius: 3px;
+        margin-right: 4px;
+    }
+    p.letra{
+        margin-right: 4px;
+        color: black;
+        font-size: 60px;
+        margin-left: 10px;
+    }
+    p.ganhou{
+        margin-right: 4px;
+        color: green;
+        font-size: 60px;
+        margin-left: 10px;
+    }
+    p.perdeu{
+    margin-right: 4px;
+    color: red;
+    font-size: 60px;
+    margin-left: 10px;
+    }
+
+
+`
---
+++ src/components/style/globalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }`
+  export default GlobalStyle;
